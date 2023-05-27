54044671ea5682df31ae82f365b413977553a9ec
Joshua Burt <joshuaadburt@gmail.com>
2023-05-26 22:20:50 -0300

    Added tests for roll.py

--- src/roll.py
+++ src/roll.py
@@ -1,17 +1,16 @@
 import random
 import re
-import discord
 from discord import option
 
 from bot import bot
 
 
 class Roll:
-    def __init__(self, num_of_rolls, operation, modifier, max_num):
+    def __init__(self, num_of_rolls, operation, modifier, faces):
         self.num_of_rolls = num_of_rolls
         self.operation = operation
         self.modifier = modifier
-        self.max_num = max_num
+        self.faces = faces
 
 
 @bot.slash_command(name="roll", description="Roll a number of various sided dice")
@@ -22,27 +21,28 @@ class Roll:
     default=0
 )
 async def roll(ctx, number_of_dice, number_of_faces, modifier):
-    if int(modifier) >= 0:
+    if int(modifier) == 0:
+        roll_str = f"{number_of_dice}d{number_of_faces}"
+    elif int(modifier) > 0:
         roll_str = f"{number_of_dice}d{number_of_faces} + {modifier}"
     else:
         roll_str = f"{number_of_dice}d{number_of_faces} - {abs(int(modifier))}"
 
-    await roll_using_notation(ctx, roll_str)
+    result = await roll_using_notation(roll_str)
+    ctx.respond(f"> {roll_str} = {str(result)}")
 
 
-async def roll_using_notation(ctx, raw_roll):
-    roll_obj = prepare_roll(raw_roll.replace(" ", ""))
-    random_num = 0
+async def roll_using_notation(raw_roll):
+    roll_obj = await prepare_roll(raw_roll.replace(" ", ""))
     modified_num = 0
     num = 0
 
     # Generate a random number using the max roll as many times as there are rolls. Add each roll together.
     if not roll_obj:
-        await ctx.respond("Invalid input")
         return False
     else:
         for i in range(0, roll_obj.num_of_rolls):
-            random_num = random.randint(1, roll_obj.max_num)
+            random_num = random.randint(1, roll_obj.faces)
             num += random_num
 
     # Add or subtract the modifier value from the random number, and set it to a new variable.
@@ -51,39 +51,16 @@ async def roll_using_notation(ctx, raw_roll):
     elif roll_obj.operation == '-':
         modified_num = num - roll_obj.modifier
 
-    if roll_obj.max_num == 2 and 1 <= modified_num <= 2:
-        await ctx.respond(file=discord.File('img\\dice\\d2\\d2-' + str(modified_num) + '.jpg'))
-        if random_num == '1':
-            await ctx.send('It\'s heads!')
-        else:
-            await ctx.send('It\'s tails!')
-        return
-
-    # Identifies the maximum roll, and if the total roll is <= to it, returns an image of that die and number
-    elif roll_obj.max_num == 4 and 1 <= modified_num <= 4:
-        await ctx.respond(file=discord.File('img\\dice\\d4\\d4-' + str(modified_num) + '.png'))
-    elif roll_obj.max_num == 6 and 1 <= modified_num <= 6:
-        await ctx.respond(file=discord.File('img\\dice\\d6\\d6-' + str(modified_num) + '.png'))
-    elif roll_obj.max_num == 8 and 1 <= modified_num <= 8:
-        await ctx.respond(file=discord.File('img\\dice\\d8\\d8-' + str(modified_num) + '.png'))
-    elif roll_obj.max_num == 10 and 1 <= modified_num <= 10:
-        await ctx.respond(file=discord.File('img\\dice\\d10\\d10-' + str(modified_num) + '.png'))
-    elif roll_obj.max_num == 12 and 1 <= modified_num <= 12:
-        await ctx.respond(file=discord.File('img\\dice\\d12\\d12-' + str(modified_num) + '.png'))
-    elif roll_obj.max_num == 20 and 1 <= modified_num <= 20:
-        await ctx.respond(file=discord.File('img\\dice\\d20\\d20-' + str(modified_num) + '.png'))
-
     # send the message containing the roll info if there was a specified modifier. Otherwise, just respond the result.
     if roll_obj.modifier != 0:
-        await ctx.respond("> " + str(num) + " " + roll_obj.operation + " "
-                          + str(roll_obj.modifier) + " = " + str(modified_num))
+        return modified_num
     else:
-        await ctx.respond("> " + str(num))
+        return num
 
 
 # Inputs a string in the format of a D&D roll (e.g. 2d20 + 2) and extracts the information and finally returns an object
 # of type Roll that contains all the important information. In the case of invalid input, returns false.
-def prepare_roll(raw_roll):
+async def prepare_roll(raw_roll):
     # Find where the 'd' in the message is to use as an anchor
     d_pos = raw_roll.find('d')
 
@@ -100,7 +77,7 @@ def prepare_roll(raw_roll):
         return False
 
     # Finds the maximum number that comes before + or -
-    max_num = int(re.findall('(?<=d)\d+(?=$|\D)', raw_roll)[0])
+    faces = int(re.findall('(?<=d)\d+(?=$|\D)', raw_roll)[0])
 
     matches = ['+', '-']
 
@@ -114,6 +91,6 @@ def prepare_roll(raw_roll):
         operation = "+"
         modifier = 0
 
-    roll_obj = Roll(num_of_rolls, operation, modifier, max_num)
+    roll_obj = Roll(num_of_rolls, operation, modifier, faces)
 
     return roll_obj
