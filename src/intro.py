efd5fbfe713e2fcd6c676f6762113dfa76f5e404
Joshua Burt <joshuaadburt@gmail.com>
2023-07-10 21:50:43 -0300

    Added command to allow uploading intros for other users

--- src/intro.py
+++ src/intro.py
@@ -26,6 +26,30 @@ async def intro(ctx):
     await ctx.respond("Your intro is now " + ("ON" if new_play_on_enter else "OFF"), ephemeral=True)
 
 
+@bot.slash_command(name="upload_other", description="Upload an .mp3 file to change someone else's intro", )
+@option(
+    "attachment",
+    discord.Attachment,
+    description=f"An .mp3 file to be used as given user's intro. Max {str(max_length)} seconds.",
+    required=True,
+)
+async def upload_others_intro(ctx: discord.ApplicationContext, attachment: discord.Attachment, username):
+    if not ctx.author.guild_permissions.administrator:
+        await ctx.respond("You must have the administrator permission to change other members' intros", ephemeral=True)
+        return
+
+    if len(username) > 0:
+        try:
+            user = await ctx.guild.fetch_member(username.strip("<@!>"))
+
+        # Intercepts an exception when a user does not provide a snowflake.
+        except discord.errors.HTTPException:
+            await ctx.respond("Please include the '@' at the start of the name of the user you wish to change",
+                              ephemeral=True)
+        else:
+            await change_intro(ctx, attachment, user)
+
+
 @bot.slash_command(name="upload", description="Upload an .mp3 file to change your intro", )
 @option(
     "attachment",
@@ -34,6 +58,10 @@ async def intro(ctx):
     required=True,
 )
 async def upload_intro(ctx: discord.ApplicationContext, attachment: discord.Attachment):
+    await change_intro(ctx, attachment, ctx.author)
+
+
+async def change_intro(ctx, attachment, user):
     if attachment.content_type == "audio/mpeg":
         file = await attachment.to_file()
 
@@ -43,14 +71,14 @@ async def upload_intro(ctx: discord.ApplicationContext, attachment: discord.Atta
             return
 
         # Apply the new file
-        file_name = await json_utils.get_user_field(ctx.author.id, "file_name")
+        file_name = await json_utils.get_user_field(user.id, "file_name")
 
         await attachment.save(Path(str(Path.cwd()) + f"/../sounds/intros/{str(file_name)}"))
 
-        await ctx.respond("Your intro has been changed")
-        await log(f"Changed {Fore.YELLOW + str(ctx.author) + Fore.RESET}'s intro")
+        await ctx.respond(f"{user.name}'s intro has been changed", ephemeral=True)
+        await log(f"Changed {Fore.YELLOW + str(user.name) + Fore.RESET}'s intro")
     else:
-        await ctx.respond("Please upload an .mp3 file")
+        await ctx.respond("Please upload an .mp3 file", ephemeral=True)
 
 
 @bot.event
